57a801b5212ccc93c47e277b1db7e488fb0ec4c2
Markos <markplusgood@gmail.com>
2025-12-11 13:59:41 +0400

    ya cf patch

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/kit';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +7,9 @@ const config = {
 
     kit: {
         adapter: adapter(),
+        alias: {
+            $lib: 'src/lib'
+        }
     }
 };
 
