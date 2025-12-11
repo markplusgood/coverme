750a2e84490f0463c780c64431acde0532cb1e76
Markos <markplusgood@gmail.com>
2025-12-11 13:32:10 +0400

    ya cf patch

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
     preprocess: vitePreprocess(),
 
     kit: {
-        // adapter-auto will automatically detect Cloudflare environment
-        // and use the appropriate adapter when deployed to Cloudflare Pages
         adapter: adapter(),
     }
 };
